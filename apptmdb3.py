2c3b31ba6a895ca3fbcc9b5d11ccb6ace3b27dce
José Costa <115988103+jcosta92@users.noreply.github.com>
2023-08-03 23:36:24 +0100

    Add files via upload

--- apptmdb3.py
+++ apptmdb3.py
@@ -96,6 +96,5 @@ def index():
     return render_template('home2.html', recommendations=recommendations)
 
 if __name__ == "__main__":
-if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
